d835a25eb783b58e57412efcc8c221118a692551
dharasydnia <djoshi@sydnia.com>
2013-03-08 16:21:54 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githubops::Application.config.secret_token = '754e03fe375fa9a59a00f994fc0400187be16bef35717b1380c269734e9383bd000fdc4afad15cb38b883179cdaf6a6367625182b23112b9b195b0d6ada92ae7'
